425182d5ce66c69c27dc12ab78a2dd1ae787aa94
rodrigo campos <rodrigodeveloper96@gmail.com>
2019-06-28 16:12:26 -0300

    final

---
+++ trash/backend/src/models/professores.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+// criando o model de professores
+const professoresSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    login:{
+        type: String,
+        required: true
+    },
+    senha: {
+        type:String,
+        required: true
+    },
+    criadoEm:{
+        type: Date,
+        default: Date.now
+    },
+   
+    
+
+})
+
+mongoose.model('professores', professoresSchema)
